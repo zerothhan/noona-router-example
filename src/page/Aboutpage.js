c8f7a3325866bcb1318aba3cc7a30e7fcfadfe55
zerothhan <zerothhan@gmail.com>
2022-08-21 17:51:30 +0900

    2. Link, Navigate

--- src/page/Aboutpage.js
+++ src/page/Aboutpage.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Aboutpage = () => {
+  const navigate = useNavigate();
+
+  const goToHomepage = () => {
+    navigate("/");
+  };
+
   return (
     <div>
       <h1>Aboutpage</h1>
+      <button onClick={goToHomepage}>Go to Homepage</button>
     </div>
   );
 };
--- src/page/Homepage.js
+++ src/page/Homepage.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Homepage = () => {
   return (
     <div>
       <h1>Homepage</h1>
+      <Link to="/about">Go to about page</Link>
     </div>
   );
 };
