beab537633f9e7d29c957024742043b1de2648a4
zerothhan <zerothhan@gmail.com>
2022-08-21 20:06:21 +0900

    5. useSearchParams

--- src/page/ProductPage.js
+++ src/page/ProductPage.js
@@ -1,6 +1,10 @@
 import React from "react";
+import { useSearchParams } from "react-router-dom";
 
 const ProductPage = () => {
+  let [query, setQuery] = useSearchParams();
+  console.log(query.get("q"));
+
   return (
     <div>
       <h1>Show All Products!!!</h1>
