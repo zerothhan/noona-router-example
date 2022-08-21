5d4fb7ded0044a911e736ec3e7cec731ffc880dd
zerothhan <zerothhan@gmail.com>
2022-08-21 18:03:57 +0900

    3. RESTful Route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import Homepage from "./page/Homepage";
 import Aboutpage from "./page/Aboutpage";
 import { Routes, Route } from "react-router-dom";
+import ProductPage from "./page/ProductPage";
+import ProductDetailPage from "./page/ProductDetailPage";
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/about" element={<Aboutpage />} />
+        <Route path="/products" element={<ProductPage />} />
+        <Route path="/products/:id" element={<ProductDetailPage />} />
       </Routes>
     </div>
   );
---
+++ src/page/ProductPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProductPage = () => {
+  return (
+    <div>
+      <h1>Show All Products!!!</h1>
+    </div>
+  );
+};
+
+export default ProductPage;
